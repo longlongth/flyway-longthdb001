afe191c6998c98fecb72ab5b0864beb65d38f5dd
longlongth <210472057+longlongth@users.noreply.github.com>
2025-07-16 09:27:39 +0700

    more changes to db creation saving

--- db-creation/createdb.sql
+++ db-creation/createdb.sql
@@ -5,3 +5,11 @@ CREATE DATABASE longthdb001
     LOCALE_PROVIDER = 'libc'
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
+
+CREATE DATABASE "longthdb001-prod"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LOCALE_PROVIDER = 'libc'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
---
+++ db-creation/createschema.sql
@@ -0,0 +1,3 @@
+CREATE SCHEMA IF NOT EXISTS longth001
+    AUTHORIZATION postgres;
+
